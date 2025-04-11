2396f3b439b422f716055c8b85a55dddfdf3fd83
Amybr01 <148042817+Amybr01@users.noreply.github.com>
2025-04-11 22:13:19 +0200

    feat: server

--- felicks-backend/config/server.ts
+++ felicks-backend/config/server.ts
@@ -1,7 +1,9 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+module.exports = ({ env }) => ({
+  url: env('RENDER_EXTERNAL_URL'), // optioneel voor online URL
+  host: '0.0.0.0',
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
 });
+
